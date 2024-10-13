04c9eb438983e380ed7fe23a43b44bdb09b40b0a
Ckola99 <Christopherkola@gmail.com>
2024-10-13 22:00:05 +0200

    refactore of bloglist

--- bloglist/models/blog.js
+++ bloglist/models/blog.js
@@ -2,25 +2,18 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-
-const noteSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    minLength: 5,
-    required: true,
-    unique: true, // Enforce uniqueness
-  },
-  important: {
-    type: Boolean,
-    default: false,
-  },
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
 })
 
 // Set the toJSON method to transform Mongoose document objects:
 // - Convert the _id object to a string and rename it to 'id' for consistency.
 // - Remove unnecessary fields like _id and __v from the returned JSON.
 // This helps simplify the data structure we send to clients and ensures the id is always a string.
-noteSchema.set('toJSON', {
+blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
@@ -30,4 +23,4 @@ noteSchema.set('toJSON', {
 
 
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model('Blog', blogSchema)
